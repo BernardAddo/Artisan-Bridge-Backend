0bcfe2c9757b7da457ea0d507f7728fc7c40cc82
Addo Bernard Tawiah <65928766+BernardAddo@users.noreply.github.com>
2021-09-06 12:46:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,40 @@ from os import error
 from flask import request, session, Response
 import json
 from wtforms_json import from_json
+from wtforms import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired, ValidationError
+# from Flaskapp import artisans, services, customers, records, db
 
+
+class LoginForm(Form):
+    customer_username = StringField(
+        'customer_username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
+class signUpForm(Form):
+
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    contact = StringField('contact', validators=[DataRequired()])
+    customer_username = StringField('username', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    password = PasswordField('password', validators=[
+                             DataRequired(), Length(min=8, max=80)])
+
+
+    def __repr__(self):
+        return f"{self.username.data, self.email.data, self.password.data}"
+
+
+class adminForm(Form):
+
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    
+    
 def admin_login_required(original):
 
     def wrapper():
